980ded9167ed3abf9d508038bf9531d1d49a7763
thanwin84 <thanwinhline84@gmail.com>
2024-09-17 20:30:18 +0600

    Moved backend files in seperate folder

---
+++ back-end/routes/auth.route.js
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import {
+    register,
+    login,
+    logout,
+    changePassword,
+    reEnterPassword,
+    toggleTwoStepAuthentication,
+    sendVerificationCode,
+    verifyVerificationCode,
+    deleteAccount
+} from '../controllers/auth.controller.js'
+import {
+    validateRegisterInput,
+    validateLoginInput,
+    validateChangePasswordInput
+} from '../middleware/validationMiddleware.js'
+import {authenticateUser} from '../middleware/auth.middleware.js'
+import upload from "../middleware/multer.middleware.js";
+
+const router = Router()
+
+
+router.post('/register', upload.single("avatar"), validateRegisterInput, register)
+
+router.post('/login',validateLoginInput, login)
+
+router.route('/logout').get(logout)
+
+router.route("/change-password").patch(authenticateUser, validateChangePasswordInput, changePassword)
+router.route("/re-enter-password").post(authenticateUser, reEnterPassword)
+router.route("/toggle-two-step-authentication").patch(authenticateUser, toggleTwoStepAuthentication)
+router.route("/send-verification-code").post(authenticateUser, sendVerificationCode)
+router.route("/verify-code").post(authenticateUser, verifyVerificationCode)
+router.route("/delete-account").delete(authenticateUser, deleteAccount)
+
+export default router
