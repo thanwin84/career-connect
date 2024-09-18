a07a2ecc3a0b25d6b841955dc1723a869cc19720
thanwin84 <thanwinhline84@gmail.com>
2024-09-18 15:29:33 +0600

    update vite.config.js and update customFetch.js

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-    baseURL: "/api/v1"
+    baseURL: import.meta.env.VITE_BASE_URL
 })
 
 export default customFetch
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
-      },
-    },
-  }
-})
+export default defineConfig({
+  plugins: [react()]
+});
