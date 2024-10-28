7c00077b349ddc268baa0ccc521ac79c3b97adf8
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2024-10-28 23:49:46 +0600

    Revert "update login controller"
    This reverts commit 88e0006c33e0925fe62242abd45d9dec9bbf21bb.

--- back-end/controllers/auth.controller.js
+++ back-end/controllers/auth.controller.js
@@ -50,7 +50,6 @@ const login = asyncHandler(async(req, res)=>{
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         expires: new Date(Date.now() + oneDay),
-        SameSite:None
 
     }
     user.password = null
