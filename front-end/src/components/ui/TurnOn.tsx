2dfe461f9d25cd3855d94b68ca40c24c4de04571
thanwin84 <thanwinhline84@gmail.com>
2024-11-27 03:46:07 +0600

    update vite.config.ts

--- front-end/src/components/ui/TurnOn.tsx
+++ front-end/src/components/ui/TurnOn.tsx
@@ -4,6 +4,6 @@ export default function TurnOn(){
     return (
         <span className="bg-green-500 dark:bg-green-400  w-14 rounded-r-full rounded-l-full flex items-center justify-end">
             <CircleIcon className="text-white text-2xl"/>
-            </span>
+        </span>
     )
 }
