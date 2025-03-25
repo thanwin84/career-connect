06711d8268f419b4c6256c29ac9dbfec43ec3c62
thanwin84 <thanwinhline84@gmail.com>
2025-03-25 07:30:57 +0600

    Add notification service and update socket CORS configuration to use appConfig

--- back-end/src/socket.ts
+++ back-end/src/socket.ts
@@ -1,12 +1,14 @@
 import http from 'http';
 import { Server } from 'socket.io';
+import { appConfig } from './config/appConfig';
 
 const onlineUsers = new Map<string, string>(); // userId: socketId
 export const startSocket = (server: http.Server) => {
   const io = new Server(server, {
     cors: {
-      origin: '*',
+      origin: appConfig.FRONT_END_BASE_URL,
       methods: ['GET', 'POST'],
+      credentials: true,
     },
   });
   io.on('connection', (socket) => {
