bb7ef96d0cc068bce39e40255e111a352d8c1e7b
thanwin84 <thanwinhline84@gmail.com>
2025-04-02 19:35:20 +0600

    Add response interceptors to customFetch for handling response data and errors

--- front-end/src/utils/customFetch.ts
+++ front-end/src/utils/customFetch.ts
@@ -1,8 +1,21 @@
-import axios from "axios";
+import axios from 'axios';
 
 const customFetch = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
 });
+
+customFetch.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response.data;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
 
 export default customFetch;
