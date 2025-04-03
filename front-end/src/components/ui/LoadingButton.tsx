df6ccdae2db2f5eea29f447108b1dd276354650a
thanwin84 <thanwinhline84@gmail.com>
2025-04-03 22:59:48 +0600

    Refactor LoadingButton component

--- front-end/src/components/ui/LoadingButton.tsx
+++ front-end/src/components/ui/LoadingButton.tsx
@@ -6,15 +6,15 @@ type Props = {
 const LoadingButton = ({ buttonText = 'loading...', className }: Props) => {
   return (
     <button
-      type="button"
+      type='button'
       disabled={true}
       className={`bg-gray-500 w-full  text-gray-300 px-4 py-2 rounded-md ${className}`}
     >
       <Spinner
-        color="text-gray-500"
-        size="h-4 w-4"
-        borderThickness="border-2"
-        className="mr-2"
+        color='text-gray-500'
+        size='h-4 w-4'
+        borderThickness='border-2'
+        className='mr-2'
       />
       {buttonText}
     </button>
