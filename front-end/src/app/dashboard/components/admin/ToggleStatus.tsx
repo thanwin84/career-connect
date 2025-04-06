6f2e681db3ecb80bc5f33713fbcc2aa74f889caa
thanwin84 <thanwinhline84@gmail.com>
2025-04-06 21:23:01 +0600

    Add animation in toggleStutus

--- front-end/src/app/dashboard/components/admin/ToggleStatus.tsx
+++ front-end/src/app/dashboard/components/admin/ToggleStatus.tsx
@@ -1,4 +1,5 @@
 import { TurnOff, TurnOn } from '@/components/ui';
+import { AnimatePresence, motion } from 'motion/react';
 
 type Props = {
   accessStatus: boolean;
@@ -11,10 +12,35 @@ export default function ToggleStatus({ accessStatus, onToggleClick }: Props) {
       type='submit'
       onClick={onToggleClick}
       aria-label={
-        accessStatus ? 'Turn off access status' : 'turn on access status'
+        accessStatus ? 'Turn off access status' : 'Turn on access status'
       }
+      className='relative w-12 h-6 rounded-full   focus:outline-none'
     >
-      {accessStatus ? <TurnOn /> : <TurnOff />}
+      <AnimatePresence initial={false}>
+        {accessStatus ? (
+          <motion.div
+            key='on'
+            initial={{ x: -4, opacity: 0 }}
+            animate={{ x: 4, opacity: 1 }}
+            exit={{ x: 0, opacity: 0 }}
+            transition={{ type: 'spring', stiffness: 100, damping: 20 }}
+            className='absolute top-0 left-0 w-6 h-6 bg-green-500 rounded-full'
+          >
+            <TurnOn />
+          </motion.div>
+        ) : (
+          <motion.div
+            key='off'
+            initial={{ x: 4, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: -4, opacity: 0 }}
+            transition={{ type: 'spring', stiffness: 100, damping: 20 }}
+            className='absolute top-0 left-0 w-6 h-6 bg-red-500 rounded-full'
+          >
+            <TurnOff />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </button>
   );
 }
